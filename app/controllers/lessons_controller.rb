16dc48936b8f49d2e5e5f5836640ab33f40212f0
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-01-28 16:55:42 +0000

    enrolled users can view lessons

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,10 +1,22 @@
 class LessonsController < ApplicationController
+	before_action :authenticate_user!
+	before_action :require_in_course
+	
 	def show
 	end
 
 	private
+
 	helper_method :current_lesson
 	def current_lesson
 		@current_lesson ||= Lesson.find(params[:id])
 	end
+
+	def require_in_course
+		if !current_lesson.section.course.user.enrolled_courses.include?(current_lesson.section.course) 
+			redirect_to course_path(current_lesson.section.course), :alert => "You cannot access this course if you are not enrolled."
+		end
+
+	end
+
 end
