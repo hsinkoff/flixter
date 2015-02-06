dc01cf2bdd500ed28e7a37f12b35c884cdd8db01
Hilary Sinkoff <hsinkoff@hotmail.com>
2015-02-06 02:31:00 +0000

    index page picture movement

--- test/controllers/courses_controller_test.rb
+++ test/controllers/courses_controller_test.rb
@@ -8,31 +8,7 @@ class CoursesControllerTest < ActionController::TestCase
 			get :show, :instructor => user, :course => course, :id => course.id
 			assert_response :success
 		end
-	test "course titles are valid" do
-		user = FactoryGirl.create(:user)
-		sign_in user
-		course = FactoryGirl.create(:course, :title => "one")
-		expected = "one"
-		actual = course.title
-		assert_equal expected, actual
-	end		
-	test "course descriptions are valid" do
-		user = FactoryGirl.create(:user)
-		sign_in user
-		course = FactoryGirl.create(:course, :description => "awesome")
-		expected="awesome"
-		actual =course.description
-		assert_equal expected, actual
-	end
-	test "course costs are valid" do
-		user=FactoryGirl.create(:user)
-		sign_in user
-		course = FactoryGirl.create(:course, :cost => 2.5)
-		expected = 2.50
-		actual = course.cost
-		assert_equal expected, actual
-	end
-
+	
   # test "the truth" do
   #   assert true
   # end
--- test/models/course_test.rb
+++ test/models/course_test.rb
@@ -4,4 +4,25 @@ class CourseTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+ test "course costs are valid" do
+		user=FactoryGirl.create(:user)
+		course = FactoryGirl.create(:course, :cost => 2.5)
+		expected = 2.50
+		actual = course.cost
+		assert_equal expected, actual
+	end	
+	test "course titles are valid" do
+		user = FactoryGirl.create(:user)
+		course = FactoryGirl.create(:course, :title => "one")
+		expected = "one"
+		actual = course.title
+		assert_equal expected, actual
+	end		
+	test "course descriptions are valid" do
+		user = FactoryGirl.create(:user)
+		course = FactoryGirl.create(:course, :description => "awesome")
+		expected="awesome"
+		actual =course.description
+		assert_equal expected, actual
+	end
 end
